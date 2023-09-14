a1ab175fc66faf8efd61ad29f0b7aa9723647902
seiedg <kinghs8749@gmail.com>
2023-09-14 11:10:54 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,13 @@ def print_hello():
 
 if __name__ == '__main__':
     print_hello()
+def add(x, y):
+a = x + y
+return a
+def print_KIT():
+print(“KIT")
+if __name__ == '__main__’:
+print_KIT()
+print( print_hello(1,2) )
+
     
